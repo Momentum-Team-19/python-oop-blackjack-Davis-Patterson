a8f62e40045f1d3c85439c4b6ff03f8a6b8ea64b
Davis Patterson <DavisFPatterson@gmail.com>
2023-08-14 21:16:51 -0400

    creted the Card and Deck classes, made the deck class generate 52 cards - one of every rank & suit

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,32 @@
-# Write your blackjack game here.
+suits = ['♠', '♥', '♦', '♣']
+ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f'{self.rank} of {self.suit}'
+
+
+class Deck: 
+    def __init__(self, suits, ranks):
+        self.cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                new_card = Card(suit, rank)
+                self.cards.append(new_card)
+    
+    def __str__(self):
+        cards_str = ', '.join(str(card) for card in self.cards)
+        return f'Deck of {len(self.cards)} cards: {cards_str}'
+    
+
+new_deck = Deck(suits, ranks)
+print(new_deck)
+
+
+
