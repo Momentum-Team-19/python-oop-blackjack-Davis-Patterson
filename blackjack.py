08068b24d69f3ed59d1e0837919fe90f3f94a235
Davis Patterson <DavisFPatterson@gmail.com>
2023-08-15 16:13:08 -0400

    added hit_me() method

--- blackjack.py
+++ blackjack.py
@@ -2,13 +2,13 @@ import random
 import os
 from card_values import CARD_VALUES
 SUITS = ['♠', '♥', '♦', '♣']
-RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
 
 class Card:
-    def __init__(self,  rank, suit):
-        self.rank = rank
+    def __init__(self, rank, suit):
         self.suit = suit
+        self.rank = rank
 
     def __str__(self):
         return f'{self.rank} {self.suit}'
@@ -40,7 +40,7 @@ class Deck:
 
 class Player:
     def __init__(self, name):
-        self.name = input('What is your name? > ')
+        self.name = name
         self.hand = []
 
     def show_hand(self):
@@ -60,8 +60,8 @@ class Game:
     def __init__(self):
         self.name = 'Blackjack'
         self.deck = Deck()
-        self.deck.shuffle()
-        self.player = Player('miles')
+        # self.deck.shuffle()
+        self.player = Player(input('What is your name? > '))
         self.dealer = Dealer()
 
     def __str__(self):
@@ -81,14 +81,53 @@ class Game:
         self.deal_card(self.dealer)
         self.player.show_hand()
         self.dealer.show_hand()
-        
-    def eval_hand(self):
-        for card in self.player.hand:
+
+    def eval_hand(self, player):
+        total = 0
+
+        for card in player.hand:
+            rank = card.rank
+            if rank in CARD_VALUES:
+                total += CARD_VALUES[rank]
+                if rank == 'A' and total >= 22:
+                    total -= 10
+
+        return total
+
+    def hit_me(self, player_total):
+        while player_total < 21:
+            hit_me = input('Do you want another card? (y/n) > ').lower().strip()
+            if hit_me == 'n':
+                print(f'No card given to {self.player}')
+                return player_total
+
+            if player_total > 21:
+                return player_total
+
+            elif hit_me == 'y':
+                self.deal_card(self.player)
+                self.player.show_hand()
+                player_total = self.eval_hand(self.player)
+                print(f"{self.player.name}'s total: {player_total}")
+                return player_total
+            
+            else:
+                ("Please enter either 'y' or 'n'")
 
     def play_game(self):
         print(f'Welcome to {self.name}!')
         print(self.deck)
         self.play_hand()
+        player_total = self.eval_hand(self.player)
+        dealer_total = self.eval_hand(self.dealer)
+        print(f"{self.player.name}'s total: {player_total}")
+        print(f"{self.dealer.name}'s total: {dealer_total}")
+        player_total = self.hit_me(player_total)
+        print(f"{self.player.name}'s total: {player_total}")
+        while dealer_total <= 16:
+            self.deal_card(self.dealer)
+            dealer_total = self.eval_hand(self.dealer)
+            print(f"{self.dealer.name}'s total: {dealer_total}")
 
 
 
